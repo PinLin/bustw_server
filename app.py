9faa5e8f4959ab5400a11f6026550e2845022448
Pin Lin <moneycat711@gmail.com>
2018-06-14 22:15:51 +0800

    change json stop architecture

--- app.py
+++ app.py
@@ -52,29 +52,17 @@ def get_stop(city, route):
     # 讀取城市對應表
     with open(sys.path[0] + '/map.json', 'r') as f:
         maps = json.load(f)
+
     # 取得該城市符合條件的所有路線
     try:
         routes = api.bus_stop(maps[city], route)
     except:
         routes = []
+
     # 處理資料
-    data = {}
+    data = []
     for route in routes:
-        # 路線名稱
-        if not route['RouteUID'] in data:
-            data[route['RouteUID']] = {}
-        data[route['RouteUID']]['routeName'] = route['RouteName']['Zh_tw']
-        # 城市
-        data[route['RouteUID']]['city'] = city
-        # 子路線
-        if not 'SubRoutes' in data[route['RouteUID']]:
-            data[route['RouteUID']]['subRoutes'] = {}
-        newUID = route['SubRouteUID'] + str(route['Direction'] if 'Direction' in route else '')
-        data[route['RouteUID']]['subRoutes'][newUID] = {
-            # 子路線名稱
-            'subRouteName': route['SubRouteName']['Zh_tw'],
-        }
-        # 停靠站
+        # 記錄停靠站
         stops = []
         for stop in route['Stops']:
             # 不停靠的站不顯示
@@ -82,7 +70,49 @@ def get_stop(city, route):
                 continue
             # 停靠站名稱
             stops.append(stop['StopName']['Zh_tw'])
-        data[route['RouteUID']]['subRoutes'][newUID]['stops'] = stops
+
+        # 在 SubRouteUID 後方加入 Direction
+        route['SubRouteUID'] += str(route['Direction'] if 'Direction' in route else '')
+
+        # 確認是否已經有該 UID 的資料
+        exist = -1
+        for i in range(len(data)):
+            # 如果 UID 相同
+            if data[i]['routeUID'] == route['RouteUID']:
+                exist = i
+                break
+
+        # 如果已經存在該 UID 的資料
+        if exist != -1:
+            # 新增子路線的資料
+            data[exist]['subRoutes'].append({
+                # 子路線辨識碼
+                'subRouteUID': route['SubRouteUID'],
+                # 子路線名稱
+                'subRouteName': route['SubRouteName']['Zh_tw'],
+                # 停靠站列表
+                'stops': stops,
+            })
+        else:
+            # 新增該路線的資料
+            data.append({
+                # 路線辨識碼
+                'routeUID': route['RouteUID'],
+                # 路線名稱
+                'routeName': route['RouteName']['Zh_tw'],
+                # 城市
+                'city': city,
+                # 子路線
+                'subRoutes': [{
+                    # 子路線辨識碼
+                    'subRouteUID': route['SubRouteUID'],
+                    # 子路線名稱
+                    'subRouteName': route['SubRouteName']['Zh_tw'],
+                    # 停靠站列表
+                    'stops': stops,
+                }],
+            })
+            
     # 回傳
     return Response(json.dumps(data, ensure_ascii=False), mimetype='application/json')
 
