39ca613834020013d72bc31fdd42e83b95da03d5
Pin Lin <moneycat711@gmail.com>
2018-11-20 19:18:10 +0800

    Remove useless code in app.py

--- app.py
+++ app.py
@@ -3,36 +3,25 @@
 import sys
 import json
 
-from flask import Flask, request, redirect, Response, jsonify
-from ptx_api import PTX
+from flask import Flask, jsonify
 
 from city import city_api
 from info import info_api
 from stop import stop_api
-import config
 
 # 初始化 Flask
 app = Flask(__name__)
-app.register_blueprint(city_api)
-app.register_blueprint(info_api)
-app.register_blueprint(stop_api)
-
-# 初始化 PTX
-ptx = PTX(config.API_ID, config.API_KEY)
-
-# 載入縣市名稱對照表
-with open(sys.path[0] + '/city.json', 'r') as f:
-    taiwan = json.load(f)
-    # 只保留 API 名稱
-    for city in taiwan:
-        taiwan[city] = taiwan[city]['code']
-
-data = {'taiwan': taiwan}
 
 
 @app.route('/v1/', strict_slashes=False)
 def welcome():
     return jsonify({'message': "Welcome to bustw_server!"})
+
+
+# 註冊藍圖
+app.register_blueprint(city_api)
+app.register_blueprint(info_api)
+app.register_blueprint(stop_api)
 
 
 def main():
