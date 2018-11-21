a889188754a1c086fcc792c8f1a8a827bbe21e7a
Pin Lin <moneycat711@gmail.com>
2018-11-21 13:32:55 +0800

    Put api code in route.py temporarily

--- bustw_server/__init__.py
+++ bustw_server/__init__.py
@@ -5,10 +5,7 @@ import json
 
 from flask import Flask, jsonify
 
-from .city import city_api
-from .info import info_api
-from .stop import stop_api
-# from route import v1_api
+from .route import v1_api
 
 # 初始化 Flask
 app = Flask(__name__)
@@ -20,10 +17,7 @@ def welcome():
 
 
 # 註冊藍圖
-app.register_blueprint(city_api)
-app.register_blueprint(info_api)
-app.register_blueprint(stop_api)
-# app.register_blueprint(v1_api)
+app.register_blueprint(v1_api)
 
 
 def main():
