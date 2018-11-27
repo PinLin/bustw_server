27ab002114d1a07700e02baad56837f7fa0a6131
Pin Lin <moneycat711@gmail.com>
2018-11-27 20:49:10 +0800

    Refactoring v1_stop

--- bustw_server/api/v1_stop.py
+++ bustw_server/api/v1_stop.py
@@ -6,6 +6,20 @@ from ..config import PTX_ID, PTX_KEY
 
 # TODO: 暫時使用
 from .v1_real import main as v1_real_main
+from .v1_time import main as v1_time_main
+
+
+def get_stop(array, uid):
+    try:
+        stops = list(map(lambda x: x['stopUID'], array))
+        index = stops.index(uid)
+        return array[index]
+
+    except ValueError:
+        return {
+            'estimateTime': 0,
+            'stopStatus': 1,
+        }
 
 
 def main(city: str, route: str) -> dict:
@@ -24,12 +38,8 @@ def main(city: str, route: str) -> dict:
     except KeyError:
         bus_stops = []
 
-    try:
-        # 從 PTX 取得該城市符合條件的所有路線到站估計資料
-        bus_times = ptx.get("/v2/Bus/EstimatedTimeOfArrival/{city}/{route}".format(city=cities[city], route=route),
-                            params={'$select': 'StopUID,EstimateTime,StopStatus'})
-    except KeyError:
-        bus_times = []
+    # 取得該城市符合條件的所有路線時間資料
+    bus_times = v1_time_main(city, route)['times']
 
     # 取得該城市符合條件的所有路線公車定位資料
     bus_reals = v1_real_main(city, route)['buses']
@@ -37,36 +47,29 @@ def main(city: str, route: str) -> dict:
     result = []
     for bus_stop in bus_stops:
         # 在 SubRouteUID 後方加入 Direction
-        try:
-            bus_stop['SubRouteUID'] += str(bus_stop['Direction'])
-        except KeyError:
-            pass
+        bus_stop['SubRouteUID'] += str(bus_stop.get('Direction') or '')
 
         # 記錄停靠站
         stop_list = []
         for stop in bus_stop['Stops']:
-            # 新增停靠站
-            stop_list.append({})
-            # 停靠站辨識碼
-            stop_list[-1]['stopUID'] = stop['StopUID']
-            # 停靠站名稱
-            stop_list[-1]['stopName'] = stop['StopName']['Zh_tw']
-            # 停靠站到站時間
-            try:
-                stop_list[-1]['estimateTime'] = bus_times[list(
-                    map(lambda x: x['StopUID'], bus_times)).index(stop['StopUID'])]['EstimateTime']
-            except KeyError:
-                stop_list[-1]['estimateTime'] = -1
-            except ValueError:
-                stop_list[-1]['estimateTime'] = -1
-            # 停靠站停靠狀態
-            try:
-                stop_list[-1]['stopStatus'] = bus_times[list(
-                    map(lambda x: x['StopUID'], bus_times)).index(stop['StopUID'])]['StopStatus']
-            except KeyError:
-                stop_list[-1]['stopStatus'] = 0
-            except ValueError:
-                stop_list[-1]['stopStatus'] = 1
+
+            # 估計時間
+            estimateTime = get_stop(bus_times, stop['StopUID']).get('estimateTime')
+            # 停靠狀態
+            stopStatus = get_stop(bus_times, stop['StopUID']).get('stopStatus')
+
+            # 新增站牌
+            stop_list.append({
+                # 站牌辨識碼
+                'stopUID': stop['StopUID'],
+                # 站牌名稱
+                'stopName': stop['StopName']['Zh_tw'],
+                # 估計時間
+                'estimateTime': estimateTime if estimateTime != None else -1,
+                # 停靠狀態
+                'stopStatus': stopStatus or 0,
+            })
+
             # 車牌號碼
             stop_list[-1]['buses'] = []
             for bus_real in bus_reals:
@@ -100,25 +103,27 @@ def main(city: str, route: str) -> dict:
                 })
         except ValueError:
             # 沒有該 UID 的資料所以新增資料
-            result.append({})
-            # 路線辨識碼
-            result[-1]['routeUID'] = bus_stop['RouteUID']
-            # 路線名稱
-            result[-1]['routeName'] = bus_stop['RouteName']['Zh_tw']
-            # 城市
-            try:
-                result[-1]['city'] = bus_stop['City']
-            except KeyError:
-                result[-1]['city'] = city
-            # 子路線
-            result[-1]['subRoutes'] = [{
-                # 子路線辨識碼
-                'subRouteUID': bus_stop['SubRouteUID'],
-                # 子路線名稱
-                'subRouteName': bus_stop['SubRouteName']['Zh_tw'].split('(')[0] + "（往" + stop_list[-1]['stopName'] + "）",
-                # 停靠站列表
-                'stops': stop_list,
-            }]
+            temp = {
+                # 路線辨識碼
+                'routeUID': bus_stop['RouteUID'],
+                # 路線名稱
+                'routeName': bus_stop['RouteName']['Zh_tw'],
+                # 城市
+                'city': bus_stop.get('City') or city,
+                # 子路線
+                'subRoutes': [
+                    {
+                        # 子路線辨識碼
+                        'subRouteUID': bus_stop['SubRouteUID'],
+                        # 子路線名稱
+                        'subRouteName': bus_stop['SubRouteName']['Zh_tw'].split('(')[0] + "（往" + stop_list[-1]['stopName'] + "）",
+                        # 停靠站列表
+                        'stops': stop_list,
+                    }
+                ]
+            }
+
+            result.append(temp)
 
     # 回傳
     return {'routes': result}
