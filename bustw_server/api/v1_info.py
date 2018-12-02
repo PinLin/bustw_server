00fc3cf99148c2ff91d74abc931b0bc0913efd0b
Pin Lin <moneycat711@gmail.com>
2018-12-03 02:29:42 +0800

    optimize speed of v1_info

--- bustw_server/api/v1_info.py
+++ bustw_server/api/v1_info.py
@@ -1,43 +1,15 @@
-import sys
-from datetime import datetime
-from ptx_api import PTX
-
 from ..utils.taiwan import taiwan
-from ..config import PTX_ID, PTX_KEY
-
-cache = {}
-
-
-def ptx_get(city: str) -> dict:
-    """從 PTX 取得資料"""
-    ptx = PTX(PTX_ID, PTX_KEY)
-
-    return ptx.get("/v2/Bus/Route/{city}".format(city=city),
-                   params={'$select': 'RouteUID,RouteName,DepartureStopNameZh,DestinationStopNameZh,City'})
+from ..utils.ptx_cache import info_cache
 
 
 def main(city: str, route: str) -> dict:
     """取得該城市符合條件的所有路線基本資料"""
-    global cache
-
     cities = {}
     data = taiwan.cities
     for key in data:
         cities[key] = data[key]['code']
 
-    # 沒有這個縣市
-    if not city in cities:
-        bus_routes = []
-    else:
-        # 快取中不存在或快取過期
-        if not city in cache or (datetime.now() - cache[city]['time']).total_seconds() > 43200:
-            # 更新快取
-            cache[city] = {
-                'time': datetime.now(),
-                'data': ptx_get(cities[city]),
-            }
-
-        bus_routes = cache[city]['data']
+    bus_routes = info_cache.get(cities[city], route or '')
 
     result = {}
     for bus_route in bus_routes:
@@ -53,10 +25,6 @@ def main(city: str, route: str) -> dict:
             # 終站名稱
             'destinationStopName': bus_route.get('DestinationStopNameZh') or '',
         }
-
-        # 如果有限制路線名稱就篩選
-        if route != None and not route in temp['routeName']:
-            continue
 
         result[temp['routeUID']] = temp
 
---
+++ bustw_server/utils/ptx_cache.py
@@ -0,0 +1,83 @@
+import threading
+from datetime import datetime
+from ptx_api import PTX
+
+from ..config import PTX_ID, PTX_KEY
+
+
+class PtxUpdater(threading.Thread):
+    def __init__(self, source: dict, cache: dict, city: str):
+        self.__url = source['url']
+        self.__ptx = source['ptx']
+        self.__params = source['params']
+        self.__cache = cache
+        self.__city = city
+
+        super().__init__()
+
+    def run(self):
+        self.__cache[self.__city] = {
+            'data': self.__ptx.get(self.__url + '/{city}'.format(city=self.__city), params=self.__params),
+            'time': datetime.now(),
+        }
+
+
+class PtxCache:
+    def __init__(self, timeout: int, url: str, params: dict=None):
+        self.__url = url
+        self.__params = params or {}
+        self.__timeout = timeout
+
+        self.__ptx = PTX(PTX_ID, PTX_KEY)
+        self.__cache = {}
+
+    def __valid(self, time: datetime) -> bool:
+        return (datetime.now() - time).total_seconds() < self.__timeout
+
+    def get(self, city: str, route: str=''):
+        """取得該城市路線資料"""
+
+        # 快取不存在或過期
+        if not city in self.__cache or not self.__valid(self.__cache[city]['time']):
+            # 先取得結果
+            result = self.__ptx.get(
+                self.__url + '/{city}/{route}'.format(city=city, route=route), params=self.__params)
+
+            # 更新快取
+            if route != '':
+                # 另開執行緒進行更新
+                PtxUpdater({
+                    'url': self.__url,
+                    'ptx': self.__ptx,
+                    'params': self.__params,
+                }, self.__cache, city).start()
+            else:
+                # 直接將結果存入
+                self.__cache[city] = {
+                    'data': result,
+                    'time': datetime.now(),
+                }
+
+        # 快取可用
+        else:
+            # 對於路線名稱的處理
+            if route != None:
+                # 篩選名稱
+                result = []
+                for temp in self.__cache[city]['data']:
+                    # 篩選
+                    if not route in temp['RouteName']['Zh_tw']:
+                        continue
+
+                    result.append(temp)
+
+            else:
+                # 不做篩選
+                result = self.__cache[city]['data']
+
+        # 回傳結果
+        return result
+
+
+info_cache = PtxCache(43200, '/v2/Bus/Route',
+                      {'$select': 'RouteUID,RouteName,DepartureStopNameZh,DestinationStopNameZh,City'})
