d56bef7785b6756e74bb5abeb85815ebb3f443a7
Pin Lin <moneycat711@gmail.com>
2018-12-04 17:25:35 +0800

    Edit version rule

--- bustw_server/route.py
+++ bustw_server/route.py
@@ -40,13 +40,14 @@ def info(city, route):
 @v1_api.route('/v1/stop/<city>/<route>/', strict_slashes=False)
 def stop(city, route):
     """取得該城市符合條件的所有路線站牌資料"""
-    if request.args.get('ver'):
-        # 新版 v1_stop
-        return jsonify(list(v1_stop.main(city, route).values()))
+    version = request.args.get('ver')
+    if not version or version == 1:
+        # v1_stop v1
+        return jsonify(v1_stop_1.main(city, route))
 
     else:
-        # 舊版 v1_stop
-        return jsonify(v1_stop_1.main(city, route))
+        # v1_stop v2
+        return jsonify(list(v1_stop.main(city, route).values()))
 
 
 @v1_api.route('/v1/real/<city>/', defaults={'route': None}, strict_slashes=False)
