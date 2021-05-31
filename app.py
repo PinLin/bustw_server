db474027e311c1f25e94af7c8d406e628ce7a3a9
Pin Lin <pinlin945@gmail.com>
2021-06-01 00:48:34 +0800

    Remove useless routes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import jsonify, request
 from flask import Flask
-from controllers.old import v1_stop_2
-from controllers.old import v1_info_1, v1_stop_1, v1_real_1, v1_time_1
+from controllers.old import v1_stop_1
 from controllers import v1_root, v1_info, v1_stop, v1_real, v1_time
 
 # 初始化 Flask
@@ -18,14 +17,7 @@ def root():
 @app.route('/v1/info/<city>/<route>', strict_slashes=False)
 def info(city, route):
     """取得該城市符合條件的所有路線基本資料"""
-    version = request.args.get('ver')
-    if not version or int(version) <= 2:
-        # v1_info v1
-        return jsonify(v1_info_1.main(city, route))
-
-    else:
-        # v1_info v3
-        return jsonify(v1_info.main(city, route))
+    return jsonify(v1_info.main(city, route))
 
 
 @app.route('/v1/stop/<city>/<route>/', strict_slashes=False)
@@ -36,10 +28,6 @@ def stop(city, route):
         # v1_stop v1
         return jsonify(v1_stop_1.main(city, route))
 
-    elif int(version) <= 2:
-        # v1_stop v2
-        return jsonify(v1_stop_2.main(city, route))
-
     else:
         # v1_stop v3
         return jsonify(v1_stop.main(city, route))
@@ -48,27 +36,13 @@ def stop(city, route):
 @app.route('/v1/real/<city>/<route>/', strict_slashes=False)
 def real(city, route):
     """取得該城市符合條件的所有路線定位資料"""
-    version = request.args.get('ver')
-    if not version or int(version) <= 2:
-        # v1_real v1
-        return jsonify(v1_real_1.main(city, route))
-
-    else:
-        # v1_real v3
-        return jsonify(v1_real.main(city, route))
+    return jsonify(v1_real.main(city, route))
 
 
 @app.route('/v1/time/<city>/<route>/', strict_slashes=False)
 def time(city, route):
     """取得該城市符合條件的所有路線時間資料"""
-    version = request.args.get('ver')
-    if not version or int(version) <= 2:
-        # v1_time v1
-        return jsonify(v1_time_1.main(city, route))
-
-    else:
-        # v1_time v3
-        return jsonify(v1_time.main(city, route))
+    return jsonify(v1_time.main(city, route))
 
 
 def main():
--- controllers/old/v1_stop_1.py
+++ controllers/old/v1_stop_1.py
@@ -1,13 +1,13 @@
-from .v1_stop_2 import main as stop_main
-from .v1_real_1 import main as real_main
-from .v1_time_1 import main as time_main
+from controllers.v1_stop import main as get_stop
+from controllers.v1_time import main as get_time
+from controllers.v1_real import main as get_real
 
 
 def main(city: str, route: str) -> dict:
     """取得該城市符合條件的所有路線站牌資料"""
-    bus_stops = stop_main(city, route)
-    bus_times = time_main(city, route)
-    bus_reals = real_main(city, route)
+    bus_stops = get_stop(city, route)['routes']
+    bus_times = get_time(city, route)['stops']
+    bus_reals = get_real(city, route)['buses']
 
     routes = bus_stops
     bus_stops = {}
